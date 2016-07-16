bb17902ef4e2325cf34eb59d0c88306439f5969f
MichaelxhJiang <michaelxhj@outlook.com>
2016-07-16 04:03:04 -0400

    uploading full mini game

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+	private PlayerController playerController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController =  gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameControllerObject == null) 
+		{
+			Debug.Log ("Cannot find 'Game Controller' script");
+		}
+
+		GameObject playerControllerObject = GameObject.FindWithTag ("Player");
+		if (playerControllerObject != null)
+		{
+			playerController =  playerControllerObject.GetComponent <PlayerController>();
+		}
+		if (playerController == null) 
+		{
+			Debug.Log ("Cannot find 'Player Controller' script");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy")) 
+		{
+			return;	
+		}
+		if (explosion != null) 
+		{
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+
+		if (other.CompareTag ("Player")) {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			playerController.lives--;
+			gameController.DisplayLives (playerController.lives);
+
+			if (playerController.lives == 0) {
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+				gameController.AddScore (scoreValue);
+				Destroy (other.gameObject);
+				Destroy (gameObject);
+				gameController.GameOver ();
+			}
+		} 
+		else if (other.tag != "Player")
+		{
+			gameController.AddScore (scoreValue);
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour 
+{
+	public float lifetime;
+
+	public void Start ()
+	{
+		Destroy (gameObject, lifetime);	
+	}
+}
